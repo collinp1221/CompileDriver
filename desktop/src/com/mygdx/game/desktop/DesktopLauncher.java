6f33476ec4867e5936997e0e6326ee99343be084
collinp1221 <56361319+collinp1221@users.noreply.github.com>
2021-03-02 11:35:08 -0500

    Add files via upload

---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,19 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.MyGdxGame;
+
+import java.io.FileNotFoundException;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) throws FileNotFoundException {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+
+		config.width=550;
+		config.height=550;
+
+		config.foregroundFPS = 60; //Set max FPS to 60
+	}
+}
